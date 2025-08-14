1dc199323256309972cc8c527a8823096408af92
Emirhan Kaya <tremirhankaya@gmail.com>
2025-08-14 13:25:43 +0300

    Add new Thymeleaf form-handling example

---
+++ thymeleaf-demo/src/test/java/com/emirhankaya/thymleafdemo/ThymleafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emirhankaya.thymleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymleafdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
